dacf63d1052048898eeb4c2325f82ccf18f67652
Shavernev Oleg <oleg.shavernev1993@gmail.com>
2023-08-26 17:18:45 +0300

    my first groovy repository

---
+++ src/Main.groovy
@@ -0,0 +1,24 @@
+//static void main(String[] args) {
+//  println "Hello world!"
+//}
+
+
+class Examole{
+  static void main(String[] args){
+//    int x = 5;
+//    double y = 12.2;
+//    float sett2 = 123123.2;
+//    BigInteger bi = 30g;
+//    BigDecimal bd = 3.5g;
+//
+//    println("Целочисленное значение: " + x)
+//    println("double значение: " + y)
+//    println("float значение: " + sett2)
+//    println("BigInteger значение: " + bi)
+//    println("BigDecimal значение: " + bd)
+
+//    String x = "Привет, Мир";
+//    println("Моя строка вот такая вот: " + x);
+  }
+}
+
