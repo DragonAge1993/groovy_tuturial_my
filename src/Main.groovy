bddc7af28402e5310188e41a71b33db970c6dceb
Shavernev Oleg <oleg.shavernev1993@gmail.com>
2023-08-26 17:25:01 +0300

    My new commit for enviroment

--- src/Main.groovy
+++ src/Main.groovy
@@ -5,20 +5,20 @@
 
 class Examole{
   static void main(String[] args){
-//    int x = 5;
-//    double y = 12.2;
-//    float sett2 = 123123.2;
-//    BigInteger bi = 30g;
-//    BigDecimal bd = 3.5g;
-//
-//    println("Целочисленное значение: " + x)
-//    println("double значение: " + y)
-//    println("float значение: " + sett2)
-//    println("BigInteger значение: " + bi)
-//    println("BigDecimal значение: " + bd)
+    int x1 = 5;
+    double y = 12.2;
+    float sett2 = 123123.2;
+    BigInteger bi = 30g;
+    BigDecimal bd = 3.5g;
 
-//    String x = "Привет, Мир";
-//    println("Моя строка вот такая вот: " + x);
+    println("Целочисленное значение: " + x1)
+    println("double значение: " + y)
+    println("float значение: " + sett2)
+    println("BigInteger значение: " + bi)
+    println("BigDecimal значение: " + bd)
+
+    String x = "Привет, Мир";
+    println("Моя строка вот такая вот: " + x);
   }
 }
 
